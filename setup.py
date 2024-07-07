ca09208da84e1b12bf40f6b070001edcaeab9a0b
elyx <elio@pascarelli.com>
2024-07-07 17:14:50 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="github-context",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "PyGithub",
